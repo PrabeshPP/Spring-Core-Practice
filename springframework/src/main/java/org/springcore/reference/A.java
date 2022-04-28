03cf639c71d3e58882b40e4005ba6c7a01a711e7
Prabesh <prabesh.bista369@gmail.com>
2022-04-28 17:19:37 +0545

    Learned to inject reference types

--- springframework/src/main/java/org/springcore/reference/A.java
+++ springframework/src/main/java/org/springcore/reference/A.java
@@ -3,4 +3,28 @@ package org.springcore.reference;
 public class A {
     private int age;
     private B bObj;
+
+    public A() {
+    }
+
+    public A(int age, B bObj) {
+        this.age = age;
+        this.bObj = bObj;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public B getbObj() {
+        return bObj;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setbObj(B bObj) {
+        this.bObj = bObj;
+    }
 }
