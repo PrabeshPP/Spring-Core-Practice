ac2c480e3db4c4dcb7ef08945b8c50b0bad182e0
Prabesh <prabesh.bista369@gmail.com>
2022-04-30 16:18:55 +0545

    Solving Ambiguity Problems with Construction Injection

--- springframework/src/main/java/org/springcore/ci/Addition.java
+++ springframework/src/main/java/org/springcore/ci/Addition.java
@@ -1,5 +1,19 @@
 package org.springcore.ci;
 
 public class Addition {
+        private int value1;
+        private int value2;
+        public Addition(int val1,int val2){
+            this.value1=val1;
+            this.value2=val2;
+            System.out.println("Constructor:int,int");
+        }
+
+        public  Addition(double val1,double val2){
+            this.value1=(int)val1;
+            this.value2=(int)val2;
+            System.out.println("Constructor:double,double");
+        }
+
 
 }
