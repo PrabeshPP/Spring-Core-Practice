62c5e55c1aa7d05c6865435679ff4c279274cdaf
Prabesh <prabesh.bista369@gmail.com>
2022-05-01 12:50:11 +0545

    lifecycle methods

--- springframework/src/main/java/org/springcore/lifecycle/TestSchool.java
+++ springframework/src/main/java/org/springcore/lifecycle/TestSchool.java
@@ -1,13 +1,15 @@
 package org.springcore.lifecycle;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSchool {
     public static void main(String[] args) {
-        ApplicationContext context=new ClassPathXmlApplicationContext("schoolLC.xml");
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext("schoolLC.xml");
         School school=(School)context.getBean("school");
         System.out.println(school);
+        context.registerShutdownHook();
     }
 
 }
