dd5f5727e708e0ab3b32f6fd33a296a3a547d201
Prabesh <prabesh.bista369@gmail.com>
2022-05-01 16:19:32 +0545

    lifecycle methods

---
+++ springframework/src/main/java/org/springcore/lifecycle/Company.java
@@ -0,0 +1,16 @@
+package org.springcore.lifecycle;
+
+public class Company {
+    private String name;
+    private double revenue;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+
+}
