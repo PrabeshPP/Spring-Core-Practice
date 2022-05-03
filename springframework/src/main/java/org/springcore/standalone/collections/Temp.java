8d70e16ff8764eac00ae21e16c739e48436bc016
Prabesh <prabesh.bista369@gmail.com>
2022-05-03 08:46:34 +0545

    Spring StandAlone Collections

--- springframework/src/main/java/org/springcore/standalone/collections/Temp.java
+++ springframework/src/main/java/org/springcore/standalone/collections/Temp.java
@@ -1,9 +1,11 @@
 package org.springcore.standalone.collections;
 
 import java.util.List;
+import java.util.Map;
 
 public class Temp {
     private List<String> friends;
+    private Map<String,String> menu;
 
     public List<String> getFriends() {
         return friends;
@@ -13,10 +15,19 @@ public class Temp {
         this.friends = friends;
     }
 
+    public Map<String, String> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Map<String, String> menu) {
+        this.menu = menu;
+    }
+
     @Override
     public String toString() {
         return "Temp{" +
                 "friends=" + friends +
+                ", menu=" + menu +
                 '}';
     }
 }
