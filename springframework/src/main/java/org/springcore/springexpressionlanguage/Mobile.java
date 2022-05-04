179419f908b34e05d853915d9533bdca03157612
Prabesh <prabesh.bista369@gmail.com>
2022-05-04 14:40:55 +0545

    Spring Expression Language

--- springframework/src/main/java/org/springcore/springexpressionlanguage/Mobile.java
+++ springframework/src/main/java/org/springcore/springexpressionlanguage/Mobile.java
@@ -1,6 +1,33 @@
 package org.springcore.springexpressionlanguage;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Mobile {
     private String name;
     private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Mobile{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
