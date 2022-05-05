08126ed623e5f806cf186aea8a69c42ac8d77cb1
Prabesh <prabesh.bista369@gmail.com>
2022-05-05 09:32:23 +0545

    Spring Configuration

--- springframework/src/main/java/org/springcore/javaconfig/JavaConfig.java
+++ springframework/src/main/java/org/springcore/javaconfig/JavaConfig.java
@@ -1,10 +1,21 @@
 package org.springcore.javaconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ComponentScan(basePackages = "org.springcore.javaconfig")
+//@ComponentScan(basePackages = "org.springcore.javaconfig")
 public class JavaConfig {
+    //using a bean annotation method
+    @Bean()
+    public About getAbout(){
+        return new About();
+    }
+    @Bean(name = "student")
+    public Student getStudent(){
+        Student student=new Student();
+        return student;
+    }
 }
--- springframework/src/main/java/org/springcore/javaconfig/Student.java
+++ springframework/src/main/java/org/springcore/javaconfig/Student.java
@@ -1,37 +1,30 @@
 package org.springcore.javaconfig;
 
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+
+@Component("student")
 public class Student {
-    @Value("Prabesh")
-    private String name;
-    @Value("20")
-    private int rollNo;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Autowired
+    @Qualifier("getAbout")
+    private About about;
 
-    public int getRollNo() {
-        return rollNo;
-    }
 
-    public void setRollNo(int rollNo) {
-        this.rollNo = rollNo;
+
+    public Student() {
+
+        this.about = about;
     }
 
     @Override
     public String toString() {
         return "Student{" +
-                "name='" + name + '\'' +
-                ", rollNo=" + rollNo +
+                "about=" + about.getAbout() +
                 '}';
     }
 }
