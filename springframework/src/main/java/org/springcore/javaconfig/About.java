59259f217ae241021a8103f32848e6f6288061b9
Prabesh <prabesh.bista369@gmail.com>
2022-05-16 16:50:26 +0545

    first app

--- springframework/src/main/java/org/springcore/javaconfig/About.java
+++
@@ -1,7 +0,0 @@
-package org.springcore.javaconfig;
-
-public class About {
-    public String getAbout(){
-        return "Hi!I am Prabesh.";
-    }
-}
--- springframework/src/main/java/org/springcore/practice/JavaConfig.java
+++
@@ -1,8 +0,0 @@
-package org.springcore.practice;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class JavaConfig {
-}
